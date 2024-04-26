14d7ea8653d563f7a691382105c8461494f18db0
blairfix <blairfix@fastmail.com>
2024-04-26 09:01:26 -0600

    xserver syntax

--- configuration.nix
+++ configuration.nix
@@ -34,13 +34,13 @@
     services.xserver.enable = true;
 
     # Enable the KDE Plasma Desktop Environment.
-    services.xserver.displayManager.sddm.enable = true;
+    services.displayManager.sddm.enable = true;
     services.xserver.desktopManager.plasma5.enable = true;
 
     # Configure keymap in X11
     services.xserver = {
 	layout = "us";
-	xkbVariant = "";
+	xkbvariant = "";
     };
 
     # Enable CUPS to print documents.
