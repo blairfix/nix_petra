710c8ff9515f9a18a58e3d3d79a50e23be4b3018
blairfix <blairfix@fastmail.com>
2025-02-05 19:32:57 -0700

    pulse

--- configuration.nix
+++ configuration.nix
@@ -49,7 +49,7 @@
     services.tailscale.enable = true;
 
     # Enable sound with pipewire.
-    hardware.pulseaudio.enable = false;
+    services.pulseaudio.enable = false;
     security.rtkit.enable = true;
     services.pipewire = {
 	enable = true;
