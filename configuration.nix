d7f6982ebef1c14a993d44d53c2757ae6a182dd5
blairfix <blairfix@fastmail.com>
2025-07-19 07:23:10 -0600

    test kde wayland

--- configuration.nix
+++ configuration.nix
@@ -18,41 +18,42 @@
     # kernel
     boot.kernelPackages = pkgs.linuxPackages_latest;
 
-    networking.hostName = "petra-laptop"; # Define your hostname.
-	# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    # host
+    networking.hostName = "petra-laptop"; 
 
-	# Configure network proxy if necessary
-	# networking.proxy.default = "http://user:password@proxy:port/";
-	# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    # networking
+    networking.networkmanager.enable = true;
 
-	# Enable networking
-	networking.networkmanager.enable = true;
-
-    # Set your time zone.
+    # time zone.
     time.timeZone = "America/Edmonton";
 
     # Select internationalisation properties.
     i18n.defaultLocale = "en_CA.UTF-8";
 
-    # Enable the X11 windowing system.
-    services.xserver.enable = true;
-
-    # Enable the KDE Plasma Desktop Environment.
-    services.displayManager.sddm.enable = true;
-    services.desktopManager.plasma6.enable = true;
-
-    # Configure keymap in X11
-    services.xserver.xkb = {
-	layout = "us";
+    # Enable the KDE x11
+    #services.xserver.enable = true;
+    #services.displayManager.sddm.enable = true;
+    #services.desktopManager.plasma6.enable = true;
+    
+    # KDE wayland
+    services = {
+      desktopManager.plasma6.enable = true;
+      displayManager.sddm.enable = true;
+      displayManager.sddm.wayland.enable = true;
     };
 
-    # Enable CUPS to print documents.
+    # Configure keymap in X11
+    #services.xserver.xkb = {
+    #    layout = "us";
+    #};
+
+    # CUPS 
     services.printing.enable = true;
 
-    # Enable tailscale
+    # tailscale
     services.tailscale.enable = true;
 
-    # Enable sound with pipewire.
+    # pipewire.
     services.pulseaudio.enable = false;
     security.rtkit.enable = true;
     services.pipewire = {
@@ -60,24 +61,6 @@
 	alsa.enable = true;
 	alsa.support32Bit = true;
 	pulse.enable = true;
-	# If you want to use JACK applications, uncomment this
-	#jack.enable = true;
-
-	# use the example session manager (no others are packaged yet so this is enabled by default,
-		# no need to redefine it in your config for now)
-	#media-session.enable = true;
-    };
-
-    # Enable touchpad support (enabled default in most desktopManager).
-    # services.xserver.libinput.enable = true;
-
-
-    # syncthing
-    services.syncthing = {
-	enable = true;
-	user = "petra";
-	dataDir = "/home/petra"; 
-	configDir = "/home/petra/.config/syncthing";
     };
 
     # steam
@@ -89,7 +72,7 @@
     };
 
 
-    # Define a user account. Don't forget to set a password with ‘passwd’.
+    # user account 
     users.users.petra = {
 	isNormalUser = true;
 	description = "petra";
@@ -111,24 +94,23 @@
 		retroarchFull
 		onlyoffice-bin
 		minetest
+		wl-clipboard
+		wayland-utils
 
 	];
     };
 
 
-
     # Allow unfree packages
     nixpkgs.config.allowUnfree = true;
 
-    # List packages installed in system profile. To search, run:
-
+    # system packages
     environment.systemPackages = with pkgs; [
 
 	borgbackup
-	    git
+	git
 
     ];
-
 
     # for bash scripts 
     services.envfs.enable = true;
